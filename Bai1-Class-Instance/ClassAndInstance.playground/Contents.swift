58455f4f81bbb1b7bf36c41fb7f338a5eff6c6b6
MacOS <tieuphuqui.protb9x@icloud.com>
2021-08-19 12:08:41 +0700

    [hhg]: added bai 1 to github

---
+++ Bai1-Class-Instance/ClassAndInstance.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+
+/* Create a new Employee class with a custom initializer that requires two string arguments: firstName and lastName. Use the arguments to initialize properties with the same names as the arguments. Display a message with the values for firstName and lastName when an instance of the class is created. Display a message with the values for firstName and lastName when an instance of the class is destroyed.
+    Create an instance of the Employee class and assign it to a variable. Check the messages printed in the Playground's Debug area. Assign a new instance of the Employee class to the previously defined variable. Check the messages printed in the Playground's Debug area.
+ */
+
+
