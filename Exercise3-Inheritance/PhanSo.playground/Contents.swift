a64a8b5f8b1091c38d9a23308cfc0744c49d5027
MacOS <tieuphuqui.protb9x@icloud.com>
2021-08-23 15:45:11 +0700

    [hhg]: changed from obj1, obj2 to object1, object2

--- Exercise3-Inheritance/PhanSo.playground/Contents.swift
+++ Exercise3-Inheritance/PhanSo.playground/Contents.swift
@@ -43,22 +43,22 @@ class Result: Fraction {
     
     func divide(fraction1: Fraction, fraction2: Fraction) -> Result {
         let fraction3 = Result()
-        fraction3.numerator   = obj1.numerator   * obj2.denominator
-        fraction3.denominator = obj1.denominator * obj2.numerator
+        fraction3.numerator   = fraction1.numerator   * fraction2.denominator
+        fraction3.denominator = fraction1.denominator * fraction2.numerator
         return fraction3
     }
 }
 
-let obj1 = Result()
-let obj2 = Result()
-var obj3 = Result()
-obj1.set(numerator: 5, denominator: 6)
-obj2.set(numerator: 1, denominator: 2)
-obj3 = obj3.add(fraction1: obj1, fraction2: obj2)
-obj3.get()
-obj3 = obj3.minus(fraction1: obj1, fraction2: obj2)
-obj3.get()
-obj3 = obj3.multiply(fraction1: obj1, fraction2: obj2)
-obj3.get()
-obj3 = obj3.divide(fraction1: obj1, fraction2: obj2)
-obj3.get()
+let object1 = Result()
+let object2 = Result()
+var object3 = Result()
+object1.set(numerator: 5, denominator: 6)
+object2.set(numerator: 1, denominator: 2)
+object3 = object3.add(fraction1: object1, fraction2: object2)
+object3.get()
+object3 = object3.minus(fraction1: object1, fraction2: object2)
+object3.get()
+object3 = object3.multiply(fraction1: object1, fraction2: object2)
+object3.get()
+object3 = object3.divide(fraction1: object1, fraction2: object2)
+object3.get()
