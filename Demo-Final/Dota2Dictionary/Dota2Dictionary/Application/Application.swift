f8ca079c4cf5f79d186255d206f425d1472e97fc
MacOS <tieuphuqui.protb9x@icloud.com>
2021-09-24 14:31:33 +0700

    [hhg]: create base code

---
+++ Demo-Final/Dota2Dictionary/Dota2Dictionary/Application/Application.swift
@@ -0,0 +1,8 @@
+//
+//  Application.swift
+//  Dota2Dictionary
+//
+//  Created by MacOS on 24/09/2021.
+//
+
+import Foundation
---
+++ Demo-Final/Dota2Dictionary/Dota2Dictionary/Application/Scene/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dota2Dictionary
+//
+//  Created by MacOS on 22/09/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
